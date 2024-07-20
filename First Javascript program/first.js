ad71beaa1641acebf8d1887e1b74e0f6243506ff
Raka-schools <yrgreat2@gmail.com>
2024-07-20 11:32:41 +0530

    Simple way to calculate
    You need to just enter your two numbers and the calculated value is there.

--- First Javascript program/first.js
+++ First Javascript program/first.js
@@ -1,4 +1,14 @@
-function findAnswer() {
-    document.getElementById("para1").innerText = "Question 2";
-    document.getElementById("answer").innerText = "This is the answer of question 2."
-}
+let x,y,sum,mul,sub,div,rem;
+x = prompt("Enter the first number");
+y = prompt("Enter the second number");
+sum = x + y;
+mul = x * y;
+sub = x - y;
+div = x / y;
+rem = x % y;
+document.getElementById("sum").innerText += sum;
+document.getElementById("mul").innerText += mul;
+document.getElementById("sub").innerText += sub;
+document.getElementById("div").innerText += div;
+document.getElementById("rem").innerText += rem;
+
